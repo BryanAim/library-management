00086e1c1227c0bb5b02f851bcfa604832528e98
Isale <isale.bryan@gmail.com>
2020-02-28 09:19:47 +0300

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <p>&copy; 2019 Online Library Management System</p>
+               </div>
+
+           </div>
+       </div>
+   </section>
---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+       <div class="container">
+           <div class="row">
+               <div class="col-md-12">
+                   <p>&copy;2019 Online library management system</p>
+               </div>
+
+           </div>
+       </div>
+   </section>
